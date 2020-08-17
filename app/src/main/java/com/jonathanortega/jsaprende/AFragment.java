8faf8fe96cd6af1f07050497bedfaa920e11db8a
Kevin Morales <kfmorales94@gmail.com>
2020-08-17 11:03:43 -0500

    Correct and incorrect answers

--- app/src/main/java/com/jonathanortega/jsaprende/AFragment.java
+++ app/src/main/java/com/jonathanortega/jsaprende/AFragment.java
@@ -1,5 +1,7 @@
 package com.jonathanortega.jsaprende;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,12 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class AFragment extends Fragment {
 
+    Boolean correct = false;
+    Button answer1;
+    Button answer2;
+    Button answer3;
+    Button answer4;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -20,35 +28,71 @@ public class AFragment extends Fragment {
         TextView question = view.findViewById(R.id.textQuestion);
         TextView score = view.findViewById(R.id.textScore);
 
-        Button answer1 = view.findViewById(R.id.answer1);
+        answer1 = view.findViewById(R.id.answer1);
         answer1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Pregunta 1");
+                answer1.setBackgroundResource(R.drawable.roundselectanswer);
+                answer1.setTextColor(getActivity().getResources().getColor(R.color.white));
+                answer2.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer2.setTextColor(getActivity().getResources().getColor(R.color.black));
+                answer3.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer3.setTextColor(getActivity().getResources().getColor(R.color.black));
+                answer4.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer4.setTextColor(getActivity().getResources().getColor(R.color.black));
+                correct = true;
             }
         });
 
-        Button answer2 = view.findViewById(R.id.answer2);
+        answer2 = view.findViewById(R.id.answer2);
         answer2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Pregunta 2");
+                answer1.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer1.setTextColor(getActivity().getResources().getColor(R.color.black));
+                answer2.setBackgroundResource(R.drawable.roundselectanswer);
+                answer2.setTextColor(getActivity().getResources().getColor(R.color.white));
+                answer3.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer3.setTextColor(getActivity().getResources().getColor(R.color.black));
+                answer4.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer4.setTextColor(getActivity().getResources().getColor(R.color.black));
+                correct = false;
             }
         });
 
-        Button answer3 = view.findViewById(R.id.answer3);
+        answer3 = view.findViewById(R.id.answer3);
         answer3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Pregunta 3");
+                answer1.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer1.setTextColor(getActivity().getResources().getColor(R.color.black));
+                answer2.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer2.setTextColor(getActivity().getResources().getColor(R.color.black));
+                answer3.setBackgroundResource(R.drawable.roundselectanswer);
+                answer3.setTextColor(getActivity().getResources().getColor(R.color.white));
+                answer4.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer4.setTextColor(getActivity().getResources().getColor(R.color.black));
+                correct = false;
             }
         });
 
-        Button answer4 = view.findViewById(R.id.answer4);
+        answer4 = view.findViewById(R.id.answer4);
         answer4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Pregunta 4");
+                answer1.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer1.setTextColor(getActivity().getResources().getColor(R.color.black));
+                answer2.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer2.setTextColor(getActivity().getResources().getColor(R.color.black));
+                answer3.setBackgroundResource(R.drawable.roundemptyanswer);
+                answer3.setTextColor(getActivity().getResources().getColor(R.color.black));
+                answer4.setBackgroundResource(R.drawable.roundselectanswer);
+                answer4.setTextColor(getActivity().getResources().getColor(R.color.white));
+                correct = false;
             }
         });
 
@@ -56,12 +100,64 @@ public class AFragment extends Fragment {
         nextQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NavHostFragment.findNavController(AFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_BFragment);
+
                 System.out.println("Siguiente pregunta");
+                if (correct == true) {
+                    alertCorrect();
+                } else if (correct == false) {
+                    alertNotCorrect();
+                }
             }
         });
 
         return view;
+    }
+
+
+    private void alertNotCorrect(){
+        new AlertDialog.Builder(getContext())
+                .setTitle("Respuesta incorrecta")
+                .setMessage("Inténtelo de nuevo")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                       resetAnswers();
+                    }
+                })
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    private void alertCorrect(){
+        new AlertDialog.Builder(getContext())
+                .setTitle("¡Respuesta correcta!")
+                .setMessage("Pasar a la siguiente pregunta")
+
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        resetAnswers();
+                        NavHostFragment.findNavController(AFragment.this)
+                                .navigate(R.id.action_SecondFragment_to_BFragment);
+                    }
+                })
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    private void resetAnswers() {
+        answer1.setBackgroundResource(R.drawable.roundemptyanswer);
+        answer1.setTextColor(getActivity().getResources().getColor(R.color.black));
+        answer2.setBackgroundResource(R.drawable.roundemptyanswer);
+        answer2.setTextColor(getActivity().getResources().getColor(R.color.black));
+        answer3.setBackgroundResource(R.drawable.roundemptyanswer);
+        answer3.setTextColor(getActivity().getResources().getColor(R.color.black));
+        answer4.setBackgroundResource(R.drawable.roundemptyanswer);
+        answer4.setTextColor(getActivity().getResources().getColor(R.color.black));
     }
 }
