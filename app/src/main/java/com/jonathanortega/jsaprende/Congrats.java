d0908e33d435f0c2a94f4a4eb3f30bc11395f0c3
Kevin Morales <kfmorales94@gmail.com>
2020-08-18 08:37:28 -0500

    Minor fixes

--- app/src/main/java/com/jonathanortega/jsaprende/Congrats.java
+++ app/src/main/java/com/jonathanortega/jsaprende/Congrats.java
@@ -2,7 +2,9 @@ package com.jonathanortega.jsaprende;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +13,23 @@ import android.view.ViewGroup;
 public class Congrats extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_congrats, container, false);
+        return inflater.inflate(R.layout.fragment_congrats, container, false);
+    }
 
-        return view;
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_finish).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(Congrats.this)
+                        .navigate(R.id.action_congrats_to_WelcomeFragment);
+            }
+        });
     }
 }
